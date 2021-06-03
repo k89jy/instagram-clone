fcf1f61ca43abadeaa512059c30928e3e7805d8d
k89jy <k89jy12@gmail.com>
2021-06-03 18:18:28 +0900

    basic setups

---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Index = () => {
+    return (
+        // login page
+        //
+        <>hello</>
+    );
+};
+
+ReactDOM.render(<Index />, document.getElementById('root'));
