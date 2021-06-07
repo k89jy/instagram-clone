a3ec70e1a139a2fca1e3f66ba6091477b0f14a0c
k89jy <k89jy12@gmail.com>
2021-06-07 13:38:34 +0900

    test

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App';
 
 const Index = () => {
-    return (
-        // login page
-        //
-        <>hello</>
-    );
+    return <App />;
 };
 
 ReactDOM.render(<Index />, document.getElementById('root'));
