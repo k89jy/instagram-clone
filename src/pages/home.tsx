e8b4eda54b850c7ed1b6ef5ac388ac452dc73765
k89jy <k89jy12@gmail.com>
2021-06-09 01:20:18 +0900

    input css 입히기

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import './../assets/css/home.css';
 import instagramWord from '../assets/img/instagram-word.png';
+import { SearchElement } from '../components/seachElement';
 export default function Home() {
     return (
         <div className="home-header">
-            <img src={instagramWord} />
-            <input type="text" placeholder="검색"></input>
+            <img className="insta-img" src={instagramWord} />
+            <SearchElement />
         </div>
     );
 }
