bec4cfca9f400540c997cd751781b9bfe2531293
k89jy <k89jy12@gmail.com>
2021-06-10 16:31:34 +0900

    검색 창 만들기 완료

---
+++ src/components/seachElement.tsx
@@ -0,0 +1,8 @@
+import '../assets/css/search.css';
+export const SearchElement = () => {
+    return (
+        <div className="search-element">
+            <input name="username" placeholder="검색" />
+        </div>
+    );
+};
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import './../assets/css/home.css';
 import instagramWord from '../assets/img/instagram-word.png';
+import outline_home_black_24dp from '../assets/img/1x/outline_home_black_24dp.png';
+import round_explore_white_24dp from '../assets/img/1x/round_explore_white_24dp.png';
 import { SearchElement } from '../components/seachElement';
 export default function Home() {
     return (
         <div className="home-header">
             <img className="insta-img" src={instagramWord} />
             <SearchElement />
+            <img src={outline_home_black_24dp} />
+            <img src={round_explore_white_24dp} />
         </div>
     );
 }
